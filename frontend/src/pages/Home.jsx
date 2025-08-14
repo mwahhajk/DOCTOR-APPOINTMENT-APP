0c3494bb635cab06392bbe82210f8547caac6c39
Wahhaj <wahhajk9@gmail.com>
2025-08-15 03:18:59 +0500

    Department Section Commited

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Biography from "../components/Biography"
+import Department from '../components/Department'
 
 function Home() {
   return (
     <div>
       <Hero title={"Welcome to Our Website"} imageUrl={"/hero.png"}/>
       <Biography imageUrl={"/about.png"}/>
-      {/* <Departments/>
-      <MessageForm/> */}
+      <Department/>
+      {/* // <MessageForm/>  */}
     </div>
   )
 }
